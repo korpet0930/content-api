a4600881b970bee0ee318bd868c7d6e2d3c0e80e
Ubuntu <adminfabmedical@agentVM.xvc0sqlm0nqerjsfgztpttdpwh.bx.internal.cloudapp.net>
2018-10-14 10:32:30 +0000

    Added Application Insights

--- config/config.js
+++ config/config.js
@@ -1,5 +1,6 @@
 exports.appSettings = {
     db: (function(){
         return process.env.MONGODB_CONNECTION || 'mongodb://localhost:27017/contentdb';
-    })()
+    })(),
+    appInsightKey: 'b4c1d040-7a4c-445f-8a6a-fae86e5aa232'
 };
